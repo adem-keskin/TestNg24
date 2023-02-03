4a3fe7d7ac11e887885ef1c51b1c73eda23d0099
Ugur Tastan <broodude26@gmail.com>
2023-02-03 23:36:14 +0000

    u

--- src/test/java/techproed/pages/HomePage.java
+++ src/test/java/techproed/pages/HomePage.java
@@ -5,5 +5,6 @@ public class HomePage {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("page1");
+        System.out.println("page2");
     }
 }
