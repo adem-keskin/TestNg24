e79c998d2a56a51dafdae0515a7ae596e9780060
HaticeBilici61 <bilicihatice1987@gmail.com>
2023-02-04 22:44:40 +0100

    e

--- src/test/java/techproed/tests/Hatice.java
+++ src/test/java/techproed/tests/Hatice.java
@@ -3,5 +3,6 @@ package techproed.tests;
 public class Hatice {
     public static void main(String[] args) {
         System.out.println("Merhaba");
+        System.out.println("basarili");
     }
 }
