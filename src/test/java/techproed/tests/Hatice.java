9b3bf4b8b4bbd013b6891e72a975bbee1bfcced9
adem-keskin <ademkeskin.de@gmail.com>
2023-02-05 18:14:17 +0100

    projeFramework

--- src/test/java/techproed/tests/Hatice.java
+++
@@ -1,8 +0,0 @@
-package techproed.tests;
-
-public class Hatice {
-    public static void main(String[] args) {
-        System.out.println("Merhaba");
-        System.out.println("basarili");
-    }
-}
