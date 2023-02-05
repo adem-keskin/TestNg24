1714180314d990b9e5f35f5195f002f732c79ed7
Hilalnigarbasan <hilalnigarbasan@outlook.com>
2023-02-05 20:24:34 +0300

    h

--- src/test/java/techproed/pages/HomePage.java
+++ src/test/java/techproed/pages/HomePage.java
@@ -3,12 +3,7 @@ package techproed.pages;
 public class HomePage {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
-        System.out.println("merhaba");
-
-
-
-
+        System.out.println("hi");
 
     }
 }
