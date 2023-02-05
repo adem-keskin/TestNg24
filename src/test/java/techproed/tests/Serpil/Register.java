51b4b525c99f1f25d9f478de884dc440ad6cd5c5
Serpil Cicek <serpilcicek433@gmail.com>
2023-02-06 00:13:39 +0100

    hello

--- src/test/java/techproed/tests/Serpil/Register.java
+++ src/test/java/techproed/tests/Serpil/Register.java
@@ -6,5 +6,7 @@ public class Register {
     public static void main(String[] args) {
 
         System.out.println("hello");
+        System.out.println("hello1");
+        System.out.println("hello2");
     }
 }
