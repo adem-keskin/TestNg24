0262debdbe832e0dbb6245f629c0746042718034
adem-keskin <ademkeskin.de@gmail.com>
2023-02-11 16:18:01 +0100

    11.02

--- src/test/java/techproed/tests/adem/US05TestCase01.java
+++ src/test/java/techproed/tests/adem/US05TestCase01.java
@@ -9,30 +9,22 @@ import techproed.utilities.Driver;
 
 public class US05TestCase01 {
 
- @Test
-    public void US05_TC01() throws InterruptedException {
+    @Test
+    public void US05_TC01() {
 
         AllureToYouHomePage allureToYouHomePage = new AllureToYouHomePage();
         MyAccountPage myAccountPage = new MyAccountPage();
 
-
         Driver.getDriver().get(ConfigReader.getProperty("allureHomePage"));
-        Thread.sleep(3000);
 
         allureToYouHomePage.myAccountButton.click();
         Assert.assertTrue(myAccountPage.myAccountText.isDisplayed());
-        //Thread.sleep(2000);
-
 
         myAccountPage.usernameBox.sendKeys(ConfigReader.getProperty("email"));
-        //Thread.sleep(2000);
 
         myAccountPage.passwordBox.sendKeys(ConfigReader.getProperty("password"));
-        //Thread.sleep(2000);
 
         myAccountPage.singInButton.submit();
-        //Thread.sleep(2000);
-
 
     }
 }
