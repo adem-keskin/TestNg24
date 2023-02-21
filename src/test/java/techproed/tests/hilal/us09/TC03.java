a0c6cbd0f435c5a424325c183d520c5bf2edd9aa
Hilalnigarbasan <hilalnigarbasan@outlook.com>
2023-02-22 00:40:23 +0300

    1

--- src/test/java/techproed/tests/hilal/us09/TC03.java
+++ src/test/java/techproed/tests/hilal/us09/TC03.java
@@ -19,5 +19,7 @@ public class TC03 {
         //09- Admin email adresini yapıştırır
         //10- Admin verification code textbox ına tıklar
         //11- Admin "Verification code sent to your email: enson.andel@foundtoo.com." uyarı mesajını alır
+
+
     }
 }
